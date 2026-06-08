cb64d11ebdcc1233063438015f60fc807a1039b0
Ecid <candicelamerand@gmail.com>
2026-06-08 11:46:47 +0200

    résolution pb

--- parallele.c
+++ parallele.c
@@ -3,88 +3,108 @@
 #include <stdbool.h>
 #include <pthread.h>
 
-#define THREAD_MAX_NUMBER 5 // to change
+#define THREAD_MAX_NUMBER 7
 
 typedef struct thread_struct
 {
     bool *tab;
-    int index;
-    int len;
-    int iterator;
+    long long start_index;
+    long long max_n;
+    long long step;
 } thread_struct;
 
 void *crible_range(void *arguments)
 {
-    struct thread_struct *args = arguments;
-    for (int i = args->index; i < args->index + args->len; i += args->iterator)
+    thread_struct *args = (thread_struct *)arguments;
+    for (long long i = args->start_index; i <= args->max_n; i += args->step)
     {
         args->tab[i] = false;
     }
+    return NULL;
 }
 
-bool *crible_parallele(int n)
+bool *crible_parallele(long long n)
 {
     bool *is_prime = (bool *)malloc((n + 1) * sizeof(bool));
-    for (int i = 0; i <= n; i++)
-        is_prime[i] = true;
+    if (is_prime == NULL)
+    {
+        perror("Erreur allocation mémoire");
+        exit(EXIT_FAILURE);
+    }
 
-    is_prime[0] = is_prime[1] = false;
+    // Initialisation correcte : tester `i` et non `n`
+    for (long long i = 0; i <= n; i++)
+        is_prime[i] = (i % 2 != 0 || i == 2);
 
-    for (int i = 2; i * i <= n; i++)
+    if (n >= 0)
+        is_prime[0] = false;
+    if (n >= 1)
+        is_prime[1] = false;
+
+    for (long long i = 3; i * i <= n; i += 2)
     {
         if (is_prime[i])
         {
             pthread_t thread_tab[THREAD_MAX_NUMBER];
-            int val = i * i;
-            int step = i;
-            int nb_elem = (n - val) / step + 1; // Nombre total d'éléments à marquer
-            int len_per_thread = nb_elem / THREAD_MAX_NUMBER;
-            int remainder = nb_elem % THREAD_MAX_NUMBER;
-
-            // Allouer un tableau d'arguments pour chaque thread
-            thread_struct *t_args = malloc(THREAD_MAX_NUMBER * sizeof(thread_struct));
+            thread_struct t_args[THREAD_MAX_NUMBER];
+            long long first_multiple = i * i;
+            long long global_step = i * THREAD_MAX_NUMBER;
 
             for (int thread_number = 0; thread_number < THREAD_MAX_NUMBER; thread_number++)
             {
                 t_args[thread_number].tab = is_prime;
-                t_args[thread_number].index = val + thread_number * len_per_thread * step;
-                t_args[thread_number].iterator = step;
-                t_args[thread_number].len = len_per_thread;
-                if (thread_number < remainder)
-                    t_args[thread_number].len++; // Distribuer le reste
+                t_args[thread_number].max_n = n;
+                t_args[thread_number].start_index = first_multiple + (thread_number * i);
+                t_args[thread_number].step = global_step;
 
-                pthread_create(&thread_tab[thread_number], NULL, &crible_range, (void *)&t_args[thread_number]);
+                if (t_args[thread_number].start_index <= n)
+                {
+                    pthread_create(&thread_tab[thread_number], NULL, &crible_range, (void *)&t_args[thread_number]);
+                }
+                else
+                {
+                    thread_tab[thread_number] = 0;
+                }
             }
 
-            // Attendre la fin de tous les threads
             for (int thread_number = 0; thread_number < THREAD_MAX_NUMBER; thread_number++)
             {
-                pthread_join(thread_tab[thread_number], NULL);
+                if (thread_tab[thread_number] != 0)
+                {
+                    pthread_join(thread_tab[thread_number], NULL);
+                }
             }
-
-            free(t_args); // Libérer la mémoire allouée pour les arguments
         }
     }
-
     return is_prime;
 }
 
-void afficher_premiers(bool *is_prime, int n)
+void afficher_premiers(bool *is_prime, long long n)
 {
-    printf("Nombres premiers jusqu'à %d :\n", n);
-    for (int i = 2; i <= n; i++)
+    printf("Nombres premiers jusqu'à %lld :\n", n);
+    for (long long i = 2; i <= n; i++)
     {
         if (is_prime[i])
-            printf("%d ", i);
+            printf("%lld ", i);
     }
     printf("\n");
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    int n = 20;
+    long long n;
+    if (argc != 2)
+        n = 20;
+    else
+        n = atoll(argv[1]);
+
     bool *is_prime = crible_parallele(n);
-    afficher_premiers(is_prime, n);
+
+    if (n <= 1000)
+        afficher_premiers(is_prime, n);
+    else
+        printf("Calcul terminé avec succès pour n = %lld.\n", n);
+
     free(is_prime);
     return 0;
 }
